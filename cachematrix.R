c1c4cdcd5f13c9a2e21326dd108ba16c24f8815d
porterhio <ryan.n.porter@gmail.com>
2015-08-23 14:45:09 -0400

    Week 2 assignment upload

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Assignment 2 - Make cached matrix and solve
+##
+
+## Make Cache Matrix (similar to HW sample)
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+## Check for cached inverse and solve
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
